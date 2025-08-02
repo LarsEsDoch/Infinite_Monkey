f7c2615db7e89233ab3d991c450883115ac4a6f1
Lars <lars.bogatz@gmail.com>
2025-08-03 01:57:20 +0200

    Refactor decide_words to support search length
    Renamed decide_words.py to interface/decide_words.py and updated decide_words to accept both best_length and best_length_search. The function now allows users to choose between recommended lengths for generating or searching, improving flexibility in word length selection.

--- src/code/interface/decide_words.py
+++ src/code/interface/decide_words.py
@@ -1,4 +1,4 @@
-def decide_words(best_length, words):
+def decide_words(best_length, best_length_search, words):
     print("\n--- Decide the number of characters ---")
     if best_length is None:
         print("Do you want to enter a custom length?")
@@ -13,11 +13,16 @@ def decide_words(best_length, words):
             words = words
         return words
     else:
-        print("Do you want to accept the recommended length? If not, it may take longer to get results.")
+        print("Do you want to accept the recommended length for generating (g) or searching (s)? If not, it may take longer to get results.")
+        print(f"Recommended length for generating: {best_length}")
+        print(f"Recommended length for searching: {best_length_search}")
         print("Or you can enter a custom length. (c)")
-        decision = input("Accept recommended length? (y/n/c): ")
-        if decision.lower().strip() == "y":
+        print("Else you use the default length. (n)")
+        decision = input("Accept recommended length? (g/s/c/n): ")
+        if decision.lower().strip() == "g":
             words = best_length
+        elif decision.lower().strip() == "s":
+            words = best_length_search
         elif decision.lower().strip() == "c":
             custom_length = input("Enter a custom length: ")
             while not custom_length.isdigit():
