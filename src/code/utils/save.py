6b8e25408dd0a7d868fa437424841c1948b2a420
Lars <lars.bogatz@gmail.com>
2025-08-03 20:27:39 +0200

    Add functions for saving large text and benchmarks
    Introduces save_large_text and save_benchmark functions to handle saving large text chunks and benchmark results to the results directory. Also ensures the results directory is created before saving found words and large text.

--- src/code/utils/save.py
+++ src/code/utils/save.py
@@ -9,7 +9,17 @@ def save_text(text):
     with open(file_path, "a", encoding="utf-8") as f:
         f.write(text)
 
+def save_large_text(chunk):
+    os.makedirs("../../results", exist_ok=True)
+    file_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../results/output.txt"))
+    with open(file_path, "a") as f:
+        f.write(chunk)
+    file_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../results/output_absolute.txt"))
+    with open(file_path, "a", encoding="utf-8") as f:
+        f.write(chunk)
+
 def save_founds(english_found_words, german_found_words):
+    os.makedirs("../../results", exist_ok=True)
     file_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../results/found_english_words.txt"))
     with open(file_path, "w", encoding="utf-8") as f:
         f.write("\n".join(sorted(english_found_words)))
@@ -17,3 +27,8 @@ def save_founds(english_found_words, german_found_words):
     file_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../results/found_german_words.txt"))
     with open(file_path, "w", encoding="utf-8") as f:
         f.write("\n".join(sorted(german_found_words)))
+
+def save_benchmark(best_length, best_length_search):
+    file_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../results/benchmark.txt"))
+    with open(file_path, "w", encoding="utf-8") as f:
+        f.write(str(best_length) + "," + str(best_length_search))
